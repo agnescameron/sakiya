eca636e98d78aa285bf46f9925f6dd2eb4faebb4
agnescameron <agnesfcameron@gmail.com>
2019-06-26 00:29:00 +0300

    mopbile site -- clicking title adds a page and *kind of* scrolls to it -- need to fix bugs

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -1,3 +1,6 @@
+var pageDepth = 0;
+var initialWidth = $(document).width();
+
 function generateList(){
     var headings = ["SAKIYA", "Board", "Events", "Projects", "Curriculum", "Community"]
     for(i=0; i<headings.length; i++){
@@ -5,17 +8,43 @@ function generateList(){
   }
 }
 
-function makeDiv(name, number){
-    // vary size for fun
-    $newdiv = $('<div/>', {
-        id: name,
-        class: "heading"
+function nextPage(name){
+    pageDepth+=1;
+    //make room for next page
+    $('#container').css({
+        'width': (pageDepth+1)*initialWidth+'px',
+    })
+
+    //generate the next page
+    $newpage = $('<div/>', {
+        id: 'page' + pageDepth,
+        class: "page",
     });
 
-    $newdiv.css({
-        'top': i*90 + 'px',
+    $newpage.css({
+        'background-color': 'yellow',
     }).appendTo( '#container' );
-    $newdiv.html(name);
+
+    $('html, body').animate({
+        scrollLeft: $('#page'+pageDepth).offset().left
+    }, 1000);
+
+    console.log("scroll left is ", $('html, body').scrollLeft())
+
+}
+
+function makeDiv(name, number){
+    // vary size for fun
+    $newheading = $('<div/>', {
+        id: name,
+        class: "heading",
+        click: function(){ nextPage(name); }
+    });
+
+    $newheading.css({
+        'top': i*90 + 'px',
+    }).appendTo( '#page0' );
+    $newheading.html(name);
 }
 
 window.onload = function() {
