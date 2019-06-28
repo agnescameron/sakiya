3d53934b3db6a76cc87a1e17f68c6481d72ec6a5
rokeby <gary.zhexi.zhang@gmail.com>
2019-06-28 15:30:01 +0300

    added segments to lines, haven't been able to make them randomly curvy yet

--- assets/js/drawing.js
+++ assets/js/drawing.js
@@ -11,22 +11,47 @@ function drawMainTree() {
 	base.top = base.top - $( '#SAKIYA' ).height()/2;
 	// repositions base.left and base.top to the center of "$SAKIYA"
 	var basePoint = new Point(base.left, base.top)
-	
-	
-	for(i=0; i<menus["main"].contents.length; i++){
-		var branch = new Path();
-		branch.strokeColor = 'black';
-		var goalDiv = menus["main"].contents[i];
+	// makes new basePoint var which is this center
 
+	for(i=0; i<menus["main"].contents.length; i++){ // count up to total number of objects in "main"
+		var branch = new Path({
+		strokeColor: 'brown',
+		strokeWidth: 20,
+		strokeCap: 'round',
+	});
+
+		// console.log("segments" + branch.segments);
+		var goalDiv = menus["main"].contents[i];
 		var goal = $('#' + goalDiv).offset();
 		goal.left =	goal.left + $( '#' + goalDiv ).width()/2;
 		goal.top =	goal.top + $( '#' + goalDiv ).height()/2;
+		var goalPoint = new Point(goal.left, goal.top);
+		// makes a center goalPoint of each of the menus with class "main"
 
-		var goalPoint = new Point(goal.left, goal.top)
 
-		branch.moveTo(basePoint)
+		var midPoint = new Point(((goalPoint.x + basePoint.x)/2), ((goalPoint.y + basePoint.y)/2));
+		var firstPoint = new Point(((1/3)*basePoint.x + (2/3)*goalPoint.x), ((1/3)*basePoint.y + (2/3)*goalPoint.y));
+		var thirdPoint = new Point(((2/3)*basePoint.x + (1/3)*goalPoint.x), ((2/3)*basePoint.y + (1/3)*goalPoint.y));
+		var fourthPoint = new Point(((1/4)*basePoint.x + (3/4)*goalPoint.x), ((1/4)*basePoint.y + (3/4)*goalPoint.y));
+
+
+		var length = branch.length;
+		var points = 12;
+
+		// var start = goalPoint;
+		// for (var i = 0; i < points; i++) 
+		// 	branch.add(start + new Point(i * goalPoint));
+
+		// branch.add(new Point(midPoint));
+		branch.fullySelected = true
+
+		branch.moveTo(basePoint);
+		branch.lineTo(firstPoint);
+		branch.lineTo(midPoint);
+		branch.lineTo(thirdPoint);
+		branch.lineTo(thirdPoint);
 		branch.lineTo(goalPoint);
-		// console.log(basePoint, goalPoint)
+
 	}
 }
 
