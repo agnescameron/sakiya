3692cf64db8d7d4e56e8458984959b8de69c5ca4
agnescameron <agnesfcameron@gmail.com>
2019-07-06 16:31:38 +0300

    very basic translation

--- assets/js/static/dictionary.js
+++ assets/js/static/dictionary.js
@@ -2,6 +2,29 @@ var dictionary = {
  	"SAKIYA": {
             'en': 'SAKIYA',
             'ar': 'ﺔﻴﻗﺎﺳ',
- },
-
+ 	},
+ 	"Team": {
+            'en': 'Team',
+            'ar': 'ﻖﻳﺮﻓ',
+ 	},
+ 	"Events": {
+            'en': 'Events',
+            'ar': 'ﺙاﺪﺣﺃ',
+ 	},
+ 	"Programs": {
+            'en': 'Programs',
+            'ar': 'ﺞﻣاﺮﺑ',
+ 	},
+ 	"Curriculum": {
+            'en': 'Curriculum',
+            'ar': 'ﺓءاﺮﻗ',
+ 	},
+ 	"Friends": {
+            'en': 'Friends',
+            'ar': 'ﺏﺎﺤﺻا',
+ 	},
+ 	"Community": {
+            'en': 'Community',
+            'ar': 'ﺔﻋﺎﻤﺟ',
+ 	},
 }
---
+++ assets/js/translate.js
@@ -0,0 +1,19 @@
+var lang = 'en'
+
+function translateText() {
+	lang = (lang === 'ar') ? 'en' : 'ar';
+
+	console.log('lang is now', lang)
+
+	for(i = 0; i< Object.keys(dictionary).length; i++){
+
+		key = Object.keys(dictionary)[i];
+		
+		if(lang === 'ar')
+			$('#'+key).html(dictionary[key].ar)
+
+		else
+			$('#'+key).html(dictionary[key].en)
+		//replace contents with arabic
+	}
+}
