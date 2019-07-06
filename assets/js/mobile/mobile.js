27d3d04d3d5f9b92b11ffba04d3fd3efb40b7c59
agnescameron <agnesfcameron@gmail.com>
2019-07-06 14:07:07 +0300

    mobile now makes text pages

--- assets/js/mobile/mobile.js
+++ assets/js/mobile/mobile.js
@@ -2,18 +2,74 @@ var pageDepth = 0;
 var initialWidth = $(window).width();
 var initialHeight = $(window).height();
 
-function generateList(headings, page){
-    for(i=0; i<headings.length; i++){
-        $newheading = $('<div/>', {
-        id: headings[i],
-        class: "heading",
-        click: function(){ makePage(this.id) }
-    }).appendTo( page );
-    $newheading.html(headings[i]);
-  }
+function generateList(menu, page){
+
+    if(menu.level === 1 || menu.contents.length === 1){
+        console.log('level is 1')
+        for(i=0; i<menu.contents.length; i++){
+            console.log('i is ', i)
+            $newheading = $('<div/>', {
+            id: menu.contents[i],
+            class: "heading",
+            click: function(){ makeTextPage(this.id, menu.level) }
+        }).appendTo( page );
+
+        $newheading.html(menu.contents[i]);
+        }
+    }
+
+    else{
+        console.log('else')
+        for(i=0; i<menu.contents.length; i++){
+            $newheading = $('<div/>', {
+            id: menu.contents[i],
+            class: "heading",
+            click: function(){ makeMenuPage(this.id) }
+        }).appendTo( page );
+
+        $newheading.html(menu.contents[i]);
+        }
+    }
 }
 
-function makePage(name){
+function makeTextPage(title, parentLevel){
+    console.log('making text page for', title, "page is ", pages[title][0])
+    
+    // parent level + 1
+    pageDepth=parentLevel+1;
+
+    $('#container').css({
+        'overflow-x': 'scroll'
+    })
+    //generate the next page
+    $newpage = $('<div/>', {
+        id: 'page' + pageDepth,
+        class: "page",
+    }).appendTo( '#container' );
+
+    page = '#page'+pageDepth;
+
+    $newpage = $('<div/>', {
+        id: title,
+        class: 'textbox',
+        click: function(){ makeTextPage(this.id, menu.level) }
+    })
+    .css({
+        width: initialWidth,
+        height: initialHeight,
+    })
+    .html(pages[title][0])
+    .appendTo( page );
+
+    $('body').animate({
+        scrollLeft: ($('#page'+pageDepth).offset()).left
+    }, 1000);
+}
+
+
+function makeMenuPage(name){
+    console.log("making menu page ", name)
+
     pageDepth=menus[name].level;
 
     $('#container').css({
@@ -37,19 +93,18 @@ function makePage(name){
 
     $( '#page'+pageDepth ).html($offsetDiv);
 
-    generateList( menus[name].contents , '#page'+pageDepth)
+    generateList( menus[name] , '#page'+pageDepth)
 
     $('body').animate({
         scrollLeft: ($('#page'+pageDepth).offset()).left
     }, 1000);
-    console.log("scrolleft is now ", $('html, body').scrollLeft())
 }
 
 window.onload = function() {
     $('#container').css({
         'overflow-x': 'hidden'
     })
-    generateList(menus["main"].contents, '#page0');
+    generateList(menus["main"], '#page0');
     
     //v weird fix, to reset scroll position on load
     //
