692fec39eb8e11d4092ac48c62f4d6ba25206868
agnescameron <agnesfcameron@gmail.com>
2019-07-08 20:39:50 +0300

    ???maaaybe finally fixed mobile scroll issues

--- assets/js/mobile/mobile.js
+++ assets/js/mobile/mobile.js
@@ -2,6 +2,8 @@ var pageDepth = 0;
 var initialWidth = $(window).width();
 var initialHeight = $(window).height();
 
+console.log('initil width is ', initialWidth, 'initial height is ', initialHeight)
+
 function generateList(menu, page){
 
     if(menu.level === 1 || menu.contents.length === 1){
@@ -45,12 +47,14 @@ function makeTextPage(title, parentLevel){
     pageDepth=parentLevel+1;
 
     $('#container').css({
-        'overflow-x': 'scroll'
+        'width': 10000 + 'px',
+        'overflow-x': 'scroll',
     })
     //generate the next page
     $newpage = $('<div/>', {
         id: 'page' + pageDepth,
         class: "page",
+        width: initialWidth + 'px',
     }).appendTo( '#container' );
 
     page = '#page'+pageDepth;
@@ -62,7 +66,7 @@ function makeTextPage(title, parentLevel){
         class: 'pageTitle'    
     })
     .css({
-        width: initialWidth,
+        width: initialWidth-10 + 'px',
     })
     .html(dictionary[title].en.title)
     .appendTo( page );
@@ -72,18 +76,18 @@ function makeTextPage(title, parentLevel){
         class: 'textbox'
     })
     .css({
-        width: initialWidth,
+        width: initialWidth-10 + 'px',
     })
     
     .appendTo( page );
 
     if(lang === 'en'){
-        $divTitle.html(dictionary[title].en.contents)
+        $divTitle.html(dictionary[title].en.title)
         $divContents.html(dictionary[title].en.contents)
     }
 
     else{
-        $divTitle.html(dictionary[title].ar.contents)
+        $divTitle.html(dictionary[title].ar.title)
         $divContents.html(dictionary[title].ar.contents)
     }
 
@@ -92,20 +96,24 @@ function makeTextPage(title, parentLevel){
     }, 1000);
 }
 
-
 function makeMenuPage(name){
     console.log("making menu page ", name)
 
     pageDepth=menus[name].level;
 
     $('#container').css({
-        'overflow-x': 'scroll'
+        'width': initialWidth*2 + 'px',
+        'height': initialHeight,
     })
     //generate the next page
     $newpage = $('<div/>', {
         id: 'page' + pageDepth,
         class: "page",
-    }).appendTo( '#container' );
+    })
+    .css({
+        'width': initialWidth+ 'px'
+    })
+    .appendTo( '#container' );
 
 
     offset = ($('#'+name).offset()).top - parseInt($('#page'+pageDepth).css('padding-top'));
@@ -121,15 +129,13 @@ function makeMenuPage(name){
 
     generateList( menus[name] , '#page'+pageDepth)
 
+
     $('body').animate({
         scrollLeft: ($('#page'+pageDepth).offset()).left
     }, 1000);
 }
 
 window.onload = function() {
-    $('#container').css({
-        'overflow-x': 'hidden'
-    })
     generateList(menus["main"], '#page0');
     
     //v weird fix, to reset scroll position on load
@@ -137,7 +143,5 @@ window.onload = function() {
     $('body').animate({
         scrollLeft: 0
     }, 1);
-
-    console.log("scrolleft is now ", $('body').scrollLeft())
 
 }
