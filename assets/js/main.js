e48ac53d4f9922ae6f5d49f757a49b7bbf6f0585
agnescameron <agnesfcameron@gmail.com>
2019-07-10 15:19:48 +0300

    fonts for github pages ++ and gets 2 separate pages from sheets

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,18 @@
 //this file gets all of the site data and parses it as json
-var pageData = [];
+var page1Data = [];
+var page2Data = [];
 
 
 async function getJSON() {
     var sheetID = '1C6J5D7rLWc7Q7SRr_Lc1bM34nE28EvYKUJko3cb8JdI';
-    var sheetUrl = 'https://spreadsheets.google.com/feeds/list/' + sheetID + '/od6/public/values?alt=json';
+    var pagesUrl = 'https://spreadsheets.google.com/feeds/list/' + sheetID + '/1/public/values?alt=json';
+    var eventsUrl = 'https://spreadsheets.google.com/feeds/list/' + sheetID + '/2/public/values?alt=json';
 
-    $.getJSON(sheetUrl, function(data){
+    $.getJSON(pagesUrl, function(data){
         var entry = data.feed.entry
 
         $(entry).each(function(){
-	        pageData.push(
+	        page1Data.push(
 	        {
 	        	"id": this["gsx$id"]["$t"],
 	        	"type": this["gsx$type"]["$t"],
@@ -21,6 +23,26 @@ async function getJSON() {
 	        	"images": this["gsx$images"]["$t"] ? this["gsx$images"]["$t"] : null,
 	        })
         });
-        console.log(pageData)
+        console.log(page1Data)
     })
+
+
+
+    $.getJSON(eventsUrl, function(data){
+        var entry = data.feed.entry
+
+        $(entry).each(function(){
+	        page2Data.push(
+	        {
+	        	"id": this["gsx$events"]["$t"],
+	        	"type": this["gsx$eeeeee"]["$t"],
+	        	"heading-ar": this["gsx$heading-ar"]["$t"],
+	        	"heading-en": this["gsx$heading-en"]["$t"],
+	        	"contents-ar": this["gsx$contents-ar"]["$t"] ? this["gsx$contents-ar"]["$t"] : null,
+	        	"contents-en": this["gsx$contents-en"]["$t"] ? this["gsx$contents-en"]["$t"] : null,
+	        	"images": this["gsx$images"]["$t"] ? this["gsx$images"]["$t"] : null,
+	        })
+        });
+        console.log(page2Data)
+    })    
 }
