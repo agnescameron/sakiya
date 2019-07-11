50b0529ad22be1e17dcf49c3e2358bca2bb8fee5
agnescameron <agnesfcameron@gmail.com>
2019-07-11 09:49:14 +0300

    solved problem by searching for filename in string, rather than using absolute path

--- assets/js/desktop/desktop.js
+++ assets/js/desktop/desktop.js
@@ -14,7 +14,7 @@ async function returnToMain () {
     for (var i = 0; i < document.styleSheets.length; i++){
         var sheet = document.styleSheets[i]
         //need to find a good way to ref this for online 
-        if(sheet.href === "http://agnescameron.github.io/sakiya/assets/css/frames.css")
+        if(sheet.href.includes("frames.css"))
             styleSheet = sheet;
     }
     $('.subframe').show();
